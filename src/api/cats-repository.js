85a3682f37ad30091bef611e440169bc884c180c
NicoPiks <ravoulus1515@gmail.com>
2025-04-12 16:44:02 +0700

    Adding a .env.example and updating some of the codes description

--- src/api/cats-repository.js
+++ src/api/cats-repository.js
@@ -1,5 +1,6 @@
-const Cat = require('../database');
+const Cat = require('../database'); //Untuk connect ke schema database
 
+//fungsi untuk membuat data cat dengan create 
 async function createCat(id, breed, origin, country_code, life_span, characteristic,
                          description, image_url, width, height, mime_type)
 {

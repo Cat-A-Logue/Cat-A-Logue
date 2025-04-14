6d3924ec6ecb83544b419671fc628faf691f9850
JOCELIN14 <jocelinlin40@gmail.com>
2025-04-15 00:32:00 +0700

    Update route : tambah put

--- src/api/cats-route.js
+++ src/api/cats-route.js
@@ -1,16 +1,24 @@
-const express = require('express');
+const express = require("express");
 
-const catsController = require('./cats-controller'); //Menyambungkan ke controller
+const catsController = require("./cats-controller"); //Menyambungkan ke controller
 
 const route = express.Router();
 
 //Untuk export berbagai macam rute yang akan digunakan nantinya
 //Setiap route harus selalu dihubungkan ke cats-controller
 module.exports = (app) => {
-    app.use('/cats', route);
-    //create a new cat
-    route.post('/', catsController.createCat);
+  app.use("/cats", route);
+  //create a new cat
+  route.post("/", catsController.createCat);
 
-    //get all cats
-    route.get('/', catsController.getAllCats);
+  //get all cats
+  route.get("/", catsController.getAllCats);
+  //delete a cat by id
+  route.delete("/:id", catsController.deleteCat);
+
+  //get cat by id
+  route.get("/:id", catsController.getCatById);
+
+  //update cat by id
+  route.put("/:id", catsController.updateCat);
 };
